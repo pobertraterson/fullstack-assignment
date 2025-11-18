7077a69a65a5aaf886102a904be3fc2af5b8c255
pobertraterson <pobraterson@icloud.com>
2025-11-18 10:30:38 +0000

    The account creation works (no salting or session_token algorithm yet

--- app/controllers/user.server.controllers.js
+++ app/controllers/user.server.controllers.js
@@ -1,5 +1,5 @@
 const joi = require('joi');
-const db = require('/database');
+const db = require('../../database');
 
 const validationSchema = joi.object({
     email: joi.string().email().max(64).required(),
@@ -13,28 +13,24 @@ const validationSchema = joi.object({
         .required(),
     // stole this from stackoverflow
     // https://stackoverflow.com/questions/77880219/how-do-i-validate-a-password-using-joi-to-ensure-that-it-contains-2-numbers-2-s
-    firstname: joi.string().min(1).max(30)
-        .pattern(/(?=(?:.*[A-Z]){2,16}).+/, 'uppercase')
-        .pattern(/(?=(?:.*[a-z]){2,16}).+/, 'lowercase')
-        .required(),
-    surname: joi.string().min(1).max(30)
-        .pattern(/(?=(?:.*[A-Z]){2,16}).+/, 'uppercase')
-        .pattern(/(?=(?:.*[a-z]){2,16}).+/, 'lowercase')
-        .required()
+    firstname: joi.string().min(1).max(30).required(),
+    surname: joi.string().min(1).max(30).required()
 })
 
 validationSchema.validate({email: "admin@robertpaterson.net", password: "DontUs3_Th!sAsAPw0rd", firstname: "Robert", surname: "Paterson"})
 
 const create_account = (req, res) => {
-    const validationRes = validationSchema.validate({email: req.body.email, password: req.body.password, firstname: req.body.firstname, surname: req.body.surname});
-    if (!validationRes.valid) {
+    const validationRes = validationSchema.validate({email: req.body.email, password: req.body.password, firstname: req.body.first_name, surname: req.body.last_name});
+    console.log(validationRes);
+    if (validationRes.valid) {
         return res.status(400).send({error: "Invalid data. Make sure your email, firstname, and surname are correct and that your password is between 8-20 characters & has 2 lowercase & uppercase letters along with 2 numbers and 2 special characters."})
     }
 
+    Math.floor(Math.random() * 4000)
     const sql = 'INSERT INTO users (user_id, email, password, first_name, last_name, salt, session_token) VALUES (?,?,?,?,?,?,?)'
 
     db.run(sql, function (err) {
-        if(err) return res.sendStatus(500).send({error: 'Server Error 500'});
+        if(err) return res.sendStatus(500).send({error: 'Server Error 500. Don\'t worry! This is our fault, not yours!'});
 
         return res.status(201).send({
             user_id: this.lastID,
@@ -42,14 +38,18 @@ const create_account = (req, res) => {
             password: req.body.password,
             first_name: req.body.first_name,
             last_name: req.body.last_name,
-            salt: this.salt,
-            session_token: this.session_token
+            // salt: this.salt,
+            // session_token: this.session_token
         })
     })
 }
 
 const login = (req, res) => {
-
+    const schema = joi.object({
+        email:joi.string().email().max(64).required(),
+        password:joi.string().required()
+    })
+    return res.status(500).send({error: "Server Error 500. Don't worry! This is our fault, not yours."});
 }
 
 const logout = (req, res) => {
