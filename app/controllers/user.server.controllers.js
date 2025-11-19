ae468ac33487091ad4cbf9d6fff4871a468e27c5
pobertraterson <pobraterson@icloud.com>
2025-11-19 23:43:59 +0000

    Unfinished login backend, but working hashing algorithm

--- app/controllers/user.server.controllers.js
+++ app/controllers/user.server.controllers.js
@@ -1,4 +1,5 @@
 const joi = require('joi');
+const bcrypt = require("bcrypt");
 const db = require('../../database');
 
 const validationSchema = joi.object({
@@ -24,34 +25,55 @@ const create_account = (req, res) => {
     console.log(validationRes);
     if (validationRes.error) {
         return res.status(400).send({error: "Invalid data. Make sure your email, firstname, and surname are correct and that your password is between 8-20 characters & has 2 lowercase & uppercase letters along with 2 numbers and 2 special characters."})
+    } else {
+        const sql = 'INSERT INTO users (user_id, email, password, first_name, last_name, salt, session_token) VALUES (?,?,?,?,?,?,?)'
+
+        db.run(sql, async function (err) {
+            if (err) return res.sendStatus(500).send({error: 'Server Error 500. Don\'t worry! This is our fault, not yours!'});
+            async function hashPassword(password) {
+                try {
+                    const hashed = await bcrypt.hash(password, 10);
+                    console.log(hashed);
+                    return hashed;
+                } catch (err) {
+                    return res.status(500).send({error: "Error while hashing password: " + err});
+                }
+            }
+
+            return res.status(201).send({
+                "user_id": this.lastID,
+                "email": req.body.email,
+                "password": await hashPassword(req.body.password),
+                "first_name": req.body.first_name,
+                "last_name": req.body.last_name,
+                "salt": 10,
+                // session_token: this.session_token
+            });
+        });
     }
-
-    Math.floor(Math.random() * 4000)
-    const sql = 'INSERT INTO users (user_id, email, password, first_name, last_name, salt, session_token) VALUES (?,?,?,?,?,?,?)'
-
-    db.run(sql, function (err) {
-        if(err) return res.sendStatus(500).send({error: 'Server Error 500. Don\'t worry! This is our fault, not yours!'});
-
-        return res.status(201).send({
-            user_id: this.lastID,
-            email: req.body.email,
-            password: req.body.password,
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            // salt: this.salt,
-            // session_token: this.session_token
-        })
-    })
 }
 
 const login = (req, res) => {
 
+    const sql = 'SELECT * FROM users WHERE email = ?';
     const schema = joi.object({
         email:joi.string().email().max(64).required(),
         password:joi.string().required()
     })
     schema.validate({email: req.body.email, password: req.body.password});
-    return res.status(500).send({error: "Server Error 500. Don't worry! This is our fault, not yours."});
+
+    db.get(sql, [req.body.email], function (err, row) {
+        if (err) return res.status(500).send({error: "Server Error 500. Don't worry! This is our fault, not yours."});
+        if (!row) {
+            return res.status(404).send({error: "No user found with that email."});
+        } else {
+            bcrypt.compare(req.body.password,row.password,function(err,result) {
+                if (err) return res.status(500).send({error: "Something went wrong. Error: " + err});
+                if (result) return res.status(200).send("Successful login");
+                if (!result) return res.status(401).send("Unauthorised");
+            });
+        }
+    });
 }
 
 const logout = (req, res) => {
